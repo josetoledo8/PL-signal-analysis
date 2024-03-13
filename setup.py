5844ffc627816bbd05c982121428772bd943a4d5
José Roberto de Toledo <jose.toledo.bc@gmail.com>
2024-03-13 09:07:35 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="pl-signal-analysis",
+    version="0.0.1",
+    author="José Roberto de Toledo",
+    author_email="jose.toledo@df.ufscar.br",
+    description="python package for analysis of PL spectra",
+    url="https://github.com/josetoledo8/PL-signal-analysis.git",
+    install_requires=['scipy', 'plotly', 'pandas', 'numpy']
+)
