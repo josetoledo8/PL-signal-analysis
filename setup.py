409c858738d2dd4093c6a5f55449ba5d29a8f43d
José Roberto de Toledo <jose.toledo.bc@gmail.com>
2024-03-13 14:37:18 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pl-signal-analysis",
-    version="0.0.1",
+    version="0.0.2",
     author="José Roberto de Toledo",
     author_email="jose.toledo@df.ufscar.br",
     description="python package for analysis of PL spectra",
